0981a5fd367534a62e3319c1ee1f5c130ccde3ba
unknown <rostik057@gmail.com>
2024-08-09 13:33:50 +0300

    !ifx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,16 +12,16 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        auth()->setUser(User::find(1));
+        if ($user = User::find(1)) {
+            auth()->setUser($user);
+        }
         Company::observe(CompanyObserver::class);
     }
 }
